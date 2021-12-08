6bbf4c5b292a015cf66809d714c3d24af1db7e22
EsterHorv <ester.124@seznam.cz>
2021-12-08 13:28:16 +0100

    refresh strategy

--- app/src/main/java/com/example/android/devbyteviewer/viewmodels/DevByteViewModel.kt
+++ app/src/main/java/com/example/android/devbyteviewer/viewmodels/DevByteViewModel.kt
@@ -17,15 +17,16 @@
 package com.example.android.devbyteviewer.viewmodels
 
 import android.app.Application
+import android.provider.MediaStore
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
+import com.example.android.devbyteviewer.database.getDatabase
 import com.example.android.devbyteviewer.domain.DevByteVideo
-import com.example.android.devbyteviewer.network.DevByteNetwork
-import com.example.android.devbyteviewer.network.asDomainModel
+import com.example.android.devbyteviewer.repository.VideosRepository
 import kotlinx.coroutines.*
 import java.io.IOException
 
@@ -40,21 +41,23 @@ import java.io.IOException
  * or fragment lifecycle events.
  */
 class DevByteViewModel(application: Application) : AndroidViewModel(application) {
+    /**
+     * The data source this ViewModel will fetch results from.
+     */
+    private val videosRepository = VideosRepository(getDatabase(application))
 
     /**
+     * A playlist of videos displayed on the screen.
+     */
+    val playlist = videosRepository.videos
+     /**
      * A playlist of videos that can be shown on the screen. This is private to avoid exposing a
      * way to set this value to observers.
-     */
-    private val _playlist = MutableLiveData<List<DevByteVideo>>()
-
+    */
     /**
      * A playlist of videos that can be shown on the screen. Views should use this to get access
      * to the data.
      */
-    val playlist: LiveData<List<DevByteVideo>>
-        get() = _playlist
-
-
 
     /**
      * Event triggered for network error. This is private to avoid exposing a
@@ -86,14 +89,32 @@ class DevByteViewModel(application: Application) : AndroidViewModel(application)
      * init{} is called immediately when this ViewModel is created.
      */
     init {
-        refreshDataFromNetwork()
+        refreshDataFromRepository()
     }
 
+    /**
+     * Refresh data from the repository. Use a coroutine launch to run in a
+     * background thread.
+     */
+    private fun refreshDataFromRepository() {
+        viewModelScope.launch {
+            try {
+                videosRepository.refreshVideos()
+                _eventNetworkError.value = false
+                _isNetworkErrorShown.value = false
+
+            } catch (networkError: IOException) {
+                // Show a Toast error message and hide the progress bar.
+                if(playlist.value.isNullOrEmpty())
+                    _eventNetworkError.value = true
+            }
+        }
+    }
     /**
      * Refresh data from network and pass it via LiveData. Use a coroutine launch to get to
      * background thread.
      */
-    private fun refreshDataFromNetwork() = viewModelScope.launch {
+    /*private fun refreshDataFromNetwork() = viewModelScope.launch {
 
         try {
              val playlist = DevByteNetwork.devbytes.getPlaylist()
@@ -106,7 +127,7 @@ class DevByteViewModel(application: Application) : AndroidViewModel(application)
             // Show a Toast error message and hide the progress bar.
             _eventNetworkError.value = true
         }
-    }
+    }*/
 
     /**
      * Resets the network error flag.
